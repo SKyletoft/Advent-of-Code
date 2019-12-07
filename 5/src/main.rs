25f882c0ecbeb99427b885d0c64e9bc4ab8538a8
Samuel Kyletoft <skyletoft@gmail.com>
2019-12-07 18:10:50 +0100

    Day 5 part one

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
         .split(',')
         .map(|number| number.parse::<i32>().unwrap())
         .collect();
-    let output = machine(1, &array);
+    let output = machine(5, &array);
     println!("{}", output);
 }
 
@@ -14,69 +14,74 @@ fn machine(input: i32, program: &[i32]) -> i32 {
     let mut output = 0;
     loop {
         let opcode = format!("{:05}", array[pos]);
-        let param_one;
-        let param_two;
+        let param_one = array[pos + 1];
+        let param_two = array[pos + 2];
+        let param_three = array[pos + 3];
+        let mut mode = [false, false, false];
         match &opcode[2..3] {
-            "0" => param_one = array[pos + 1],
-            "1" => param_one = array[array[(pos + 1)] as usize],
+            "0" => mode[0] = true,
+            "1" => mode[0] = false,
             _ => panic!(),
         }
         match &opcode[1..2] {
-            "0" => param_two = array[pos + 2],
-            "1" => param_two = array[array[(pos + 2)] as usize],
+            "0" => mode[1] = true,
+            "1" => mode[1] = false,
             _ => panic!(),
         }
-
+        match &opcode[0..1] {
+            "0" => mode[2] = true,
+            "1" => mode[2] = false,
+            _ => panic!(),
+        }
         let op = &opcode[3..5];
-        let x = 5;
+        let _x = 5;
         match op {
             "01" => {
-                let sum = param_one + param_two;
-                match &opcode[0..1] {
-                    "0" => array[pos + 3] = sum,
-                    "1" => {
-                        let first = array[(pos + 3)] as usize;
-                        array[first] = sum;
-                    },
-                    _ => panic!(),
+                let first = load(mode[0], param_one, &array);
+                let second = load(mode[1], param_two, &array);
+                let sum = first + second;
+                if mode[2] {
+                    array[param_three as usize] = sum;
+                } else {
+                    let position = array[param_three as usize] as usize;
+                    array[position] = sum;
                 }
                 pos += 4;
-            },
+            }
             "02" => {
-                let product = param_one * param_two;
-                match &opcode[0..1] {
-                    "0" => array[pos + 3] = product,
-                    "1" => {
-                        let first = array[(pos + 3)] as usize;
-                        array[first] = product;
-                    },
-                    _ => panic!(),
+                let first = load(mode[0], param_one, &array);
+                let second = load(mode[1], param_two, &array);
+                let sum = first * second;
+                if mode[2] {
+                    array[param_three as usize] = sum;
+                } else {
+                    let position = array[param_three as usize] as usize;
+                    array[position] = sum;
                 }
                 pos += 4;
-            },
+            }
             "03" => {
-                let adr;
-                match &opcode[0..1] {
-                    "0" => adr = array[pos + 3],
-                    "1" => adr = array[array[(pos + 3)] as usize],
-                    _ => panic!(),
-                }
-                array[adr as usize] = input;
+                array[param_one as usize] = input;
                 pos += 2
-            },
+            }
             "04" => {
-                let adr;
-                match &opcode[0..1] {
-                    "0" => adr = array[pos + 3],
-                    "1" => adr = array[array[(pos + 3)] as usize],
-                    _ => panic!(),
-                }
-                output = array[adr as usize];
+                output = array[param_one as usize];
                 pos += 2
-            },
+            }
+            "05" => {
+                
+            }
             "99" => break,
-            _ => panic!()
+            _ => panic!(),
         }
     }
     output
+}
+
+fn load (mode_bool: bool, param: i32, array: &[i32]) -> i32 {
+    if mode_bool {
+        array[param as usize]
+    } else {
+        param
+    }
 }
