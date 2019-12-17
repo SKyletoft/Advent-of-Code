92bbd6773336b74b1f8074a8fdc390607967bf9b
Samuel Kyletoft <skyletoft@gmail.com>
2019-12-17 19:13:29 +0100

    Finished rewrite of intcode

--- intcode_rewrite/src/old.rs
+++ intcode_rewrite/src/old.rs
@@ -1,22 +1,24 @@
 use std::collections::HashMap;
 
 fn main() {
-    let input = include_str!("input7.txt");
-    let array: Vec<i64> = input
-        .split(',')
-        .map(|number| number.parse::<i64>().unwrap())
-        .collect();
-    let mut program = HashMap::new();
-    for (index, data) in array.iter().enumerate() {
-        program.insert(index, *data);
+    for _ in 0..10000 {
+        let input = include_str!("input7.txt");
+        let array: Vec<i64> = input
+            .split(',')
+            .map(|number| number.parse::<i64>().unwrap())
+            .collect();
+        let mut program = HashMap::new();
+        for (index, data) in array.iter().enumerate() {
+            program.insert(index, *data);
+        }
+        part_one(&program);
     }
-    part_one(&program);
 }
 
 fn part_one(program: &HashMap<usize, i64>) {
     let mut out = Vec::new();
     machine(&mut vec![1], &mut program.clone(), &mut out, &mut 0, &mut 0);
-    println!("{:?}", out);
+    //println!("{:?}", out);
 }
 
 fn machine(
@@ -37,7 +39,21 @@ fn machine(
             { *(&opcode[0..1].parse::<i64>().unwrap_or(0)) },
         ];
         let op = &opcode[3..5];
-        //print!("{:03}: ", *pos);
+        
+        let mut array = Vec::new();
+        for (index, value) in program.iter() {
+            while array.len() <= *index {
+                array.push(0);
+            }
+            array[*index] = *value;
+        }
+
+        /*println!("mem: {:?}, in: {:?}, out: {:?}, instruction_pointer: {}, relative_base: {}",
+            array, input, output, *pos, *rel_base
+        );*/
+
+        //print!("\n{:03}: ", *pos);
+
         match op {
             "01" => {
                 let first = load(mode[0], param_one, program, rel_base);
@@ -45,7 +61,7 @@ fn machine(
                 let sum = first + second;
                 position_write(mode[2], program, param_three, sum, *rel_base);
                 *pos += 4;
-                println!("add {}, {} => {}", first, second, param_three);
+                //println!("add {}, {} => {}", first, second, param_three);
                 continue;
             }
             "02" => {
@@ -54,13 +70,13 @@ fn machine(
                 let sum = first * second;
                 position_write(mode[2], program, param_three, sum, *rel_base);
                 *pos += 4;
-                println!("mul {}, {} => {}", first, second, param_three);
+                //println!("mul {}, {} => {}", first, second, param_three);
                 continue;
             }
             "03" => {
                 if input.len() > 0 {
                     position_write(mode[0], program, 0, input[0], *rel_base);
-                    println!("in {}", input[0]);
+                    //println!("in {}", input[0]);
                     input.remove(0);
                     *pos += 2;
                     continue;
@@ -70,7 +86,7 @@ fn machine(
             }
             "04" => {
                 output.push(load(mode[0], param_one, program, rel_base));
-                println!("\t\t\tOUT: {}",output[output.len() - 1]);
+                //println!("\t\t\tOUT: {}",output[output.len() - 1]);
                 *pos += 2;
                 continue;
             }
@@ -78,10 +94,10 @@ fn machine(
                 let first = load(mode[0], param_one, program, rel_base);
                 let second = load(mode[1], param_two, program, rel_base);
                 if first != 0 {
-                    println!("jmp if true {} {} (JUMPED)", first, second);
+                    //println!("jmp if true {} {} (JUMPED)", first, second);
                     *pos = second as usize;
                 } else {
-                    println!("jmp if true {} {} (DIDN'T JUMP)", first, second);
+                    //println!("jmp if true {} {} (DIDN'T JUMP)", first, second);
                     *pos += 3;
                 }
                 continue;
@@ -90,10 +106,10 @@ fn machine(
                 let first = load(mode[0], param_one, program, rel_base);
                 let second = load(mode[1], param_two, program, rel_base);
                 if first == 0 {
-                    println!("jmp if false {} {} (JUMPED)", first, second);
+                    //println!("jmp if false {} {} (JUMPED)", first, second);
                     *pos = second as usize;
                 } else {
-                    println!("jmp if false {} {} (DIDN'T JUMP)", first, second);
+                    //println!("jmp if false {} {} (DIDN'T JUMP)", first, second);
                     *pos += 3;
                 }
                 continue;
@@ -102,7 +118,7 @@ fn machine(
                 let first = load(mode[0], param_one, program, rel_base);
                 let second = load(mode[1], param_two, program, rel_base);
                 let position = position_load(mode[2], program, *pos, *rel_base);
-                println!("cmp {} {} ({}) => {}", first, second, first < second, position);
+                //println!("cmp {} {} ({}) => {}", first, second, first < second, position);
                 if first < second {
                     program.insert(position, 1);
                 } else {
@@ -115,7 +131,7 @@ fn machine(
                 let first = load(mode[0], param_one, program, rel_base);
                 let second = load(mode[1], param_two, program, rel_base);
                 let position = position_load(mode[2], program, *pos, *rel_base);
-                println!("eq {} {} ({}) => {}", first, second, first == second, position);
+                //println!("eq {} {} ({}) => {}", first, second, first == second, position);
                 if first == second {
                     program.insert(position, 1);
                 } else {
@@ -126,14 +142,14 @@ fn machine(
             }
             "09" => {
                 let first = load(mode[0], param_one, program, rel_base);
-                print!("rel {} {}", rel_base, first);
+                //print!("rel {} {}", rel_base, first);
                 *rel_base = (*rel_base as i64 + first) as usize;
-                println!(" ({})", rel_base);
+                //println!(" ({})", rel_base);
                 *pos += 2;
                 continue;
             }
             "99" => {
-                println!("END");
+                //println!("END");
                 return;
             }
             _ => panic!("invalid instruction"),
